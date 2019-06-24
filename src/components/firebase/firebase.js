5be0307befe09c9e4460c36e94e5e8f61199ab03
arjavibahety <arjavibahety@gmail.com>
2019-06-24 12:19:39 +0700

    uploading firebase.js

---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,63 @@
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/firestore";
+import config from "./config";
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+    this.fieldValue = app.firestore.FieldValue;
+    this.auth = app.auth();
+    this.db = app.firestore();
+    this.googleProvider = new app.auth.GoogleAuthProvider();
+  }
+
+  onAuthUserListener = (next, fallback) =>
+    this.auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        this.user(authUser.uid)
+          .get()
+          .then(snapshot => {
+            const dbUser = snapshot.data();
+
+            // default empty roles
+            if (!dbUser.roles) {
+              dbUser.roles = {};
+            }
+
+            // merge auth and db user
+            authUser = {
+              uid: authUser.uid,
+              email: authUser.email,
+              ...dbUser
+            };
+
+            next(authUser);
+          });
+      } else {
+        fallback();
+      }
+    });
+
+  //   Authentication
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignInWithGoogle = () => this.auth.signInWithPopup(this.googleProvider);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  user = uid => this.db.doc(`users/${uid}`);
+
+  users = () => this.db.collection("users");
+}
+
+export default Firebase;
