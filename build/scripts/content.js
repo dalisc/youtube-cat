2590fb41f48a7cd3b4b70ff0af57be5958ffa9e3
dalisc <42921826+dalisc@users.noreply.github.com>
2019-06-28 01:09:23 +0700

    added .config and removed react-scripts manually

--- build/scripts/content.js
+++ build/scripts/content.js
@@ -1,74 +1,59 @@
-// chrome.runtime.sendMessage({
-//   todo: "showPageAction"
-// });
+var vidID = youtube_parser(window.location.href);
+
+chrome.runtime.sendMessage({
+        userAction: "forcedRefresh",
+        vidID: vidID
+    },
+    function(response) {
+        console.log(reponse.feedback);
+    });
 
 const port = chrome.runtime.connect();
 
 port.onMessage.addListener(msg => {
-  switch (msg.type) {
-    case "reloadPage": {
-      console.log("received message");
-      document.location.reload();
+    switch (msg.type) {
+        case "reloadPage":
+            {
+                console.log("received message");
+                document.location.reload();
+            }
+        default:
+            break;
     }
-    default:
-      break;
-  }
 });
 
 // detect video ID
 function youtube_parser(url) {
-  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-  var match = url.match(regExp);
-  return match && match[7].length == 11 ? match[7] : false;
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return match && match[7].length == 11 ? match[7] : false;
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, response) {
-  if (request.todo == "changePreferences") {
-    const categoriesArr = request.categories;
-    console.log("categoriesArr: ", categoriesArr);
-    chrome.runtime.sendMessage({
-      todo: "blockedCatArr",
-      categories: categoriesArr
-    });
-  } else if (request.todo == "getVidID") {
-    console.log("content received message");
-    var vidID = youtube_parser(window.location.href);
-    response({ vidID: vidID });
-    console.log("content sent back vidID");
-  } else if (request.todo == "blockVideo") {
-    blockVideo();
-    response({ done: "done" });
-  }
+    if (request.todo == "changePreferences") {
+        const categoriesArr = request.categories;
+        console.log("categoriesArr: ", categoriesArr);
+        chrome.runtime.sendMessage({
+            todo: "blockedCatArr",
+            categories: categoriesArr
+        });
+    } else if (request.todo == "getVidID") {
+        console.log("content received message");
+        var vidID = youtube_parser(window.location.href);
+        response({ vidID: vidID });
+        console.log("content sent back vidID");
+    } else if (request.todo == "blockVideo") {
+        blockVideo();
+        response({ done: "done" });
+    } else if (request.todo == "refreshPageToUnblockVid") {
+        response({ done: "page will reload" });
+        window.location.reload();
+
+    }
 });
 
-function timeoutVidSrc(vid) {
-  vid.src = "";
-}
 
 function blockVideo() {
-  var vid = document.getElementsByTagName("video");
-  console.log("starting: ", vid);
-
-  if (vid.length > 0) {
-    for (var i = 0, l = vid.length; i < l; i++) {
-      console.log(vid[i].src);
-      vid[i].src = "";
-      console.log(vid[i].src);
-      setTimeout(timeoutVidSrc(vid[i]), 1000);
-    }
-
-    // setTimeout(function() {
-    //   var vid = document.getElementsByTagName("video");
-
-    //   console.log("first timeout function");
-
-    //   if (vid.length > 0) {
-    //     for (var i = 0, l = vid.length; i < l; i++) {
-    //       console.log(vid[i].src);
-    //       vid[i].src = "";
-    //       console.log(vid[i].src);
-    //     }
-    //   }
-    // }, 5000);
-  }
+    var vidDiv = document.getElementById("movie_player");
+    vidDiv.innerHTML = '';
 }
